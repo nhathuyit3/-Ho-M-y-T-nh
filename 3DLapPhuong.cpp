27b624c8ef4c2d9f7ffab14c20ba31519d424c4a
Phan Văn Hà <38199580+phanvha@users.noreply.github.com>
2019-10-28 11:12:56 +0700

    Tịnh tiến

--- 3DLapPhuong.cpp
+++ 3DLapPhuong.cpp
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <conio.h>
 #include <graphics.h>
-
-int main() {
-	int gd = DETECT,gm;
-	int x1,y1,x2,y2,x3,y3,x4,y4,y5,x5,x6,y6,x7,y7,x8,y8;
-	initgraph(&gd, &gm,"C:\\TC\\BGI");
-//	for(i=1;i<9;i++)
-//	{
-//		printf("Enter x%d & y%d",i,i);
-//		scanf("%d",&x[i]);
-//		scanf("%d",&y[i]);
-//	}
+int x1,y1,x2,y2,x3,y3,x4,y4,y5,x5,x6,y6,x7,y7,x8,y8;
+void velapphuong()
+{
 	x1=200,y1=200;
 	x2=300,y2=200;
 	x3=300,y3=300;
@@ -20,14 +12,16 @@ int main() {
 	x6=350,y6=150;
 	x7=350,y7=250;
 	x8=250,y8=250;
-	setcolor(YELLOW);
+	setcolor(BLUE);
 	rectangle(x5,y5,x7,y7);
 	rectangle(x1,y1,x3,y3);
 	line(x1,y1,x5,y5);
 	line(x2,y2,x6,y6);
 	line(x3,y3,x7,y7);
 	line(x4,y4,x8,y8);
-	
+}
+void tinhtien()
+{
 	int x,y;
 	printf("Enter x & y");
 	scanf("%d",&x);
@@ -38,5 +32,14 @@ int main() {
 	line(x2+x,y2+y,x6+x,y6+y);
 	line(x3+x,y3+y,x7+x,y7+y);
 	line(x4+x,y4+y,x8+x,y8+y);
+}
+int main() 
+{
+	int gd = DETECT,gm;
+	initgraph(&gd, &gm,"C:\\TC\\BGI");
+	
+	velapphuong();
+	tinhtien();
 	getch();
+	return 0;
 }
